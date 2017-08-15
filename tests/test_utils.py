219f996ed382d58c8d0be6737753bccef3d048a3
Sergey Popinevskiy <gurza000@gmail.com>
2017-08-15 09:56:35 +0300

    Implemented test for generate_signature() util

---
+++ tests/test_utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from hashlib import md5
+import random, string
+from ambassadress.utils import generate_signature
+
+
+def random_string(length):
+    """Returns random alphanumeric string in lowercase."""
+    return ''.join(random.choice(string.lowercase + string.digits) for _ in range(length))
+
+
+class TestSignatureGenerator:
+
+    def test_generate_signature(self):
+        api_key = random_string(40)
+        params = {
+            'a': random_string(40),
+            'b': random_string(40),
+            'c': random_string(40),
+            'z': 42,
+        }
+        assert md5(''.join([params['a'], params['b'], params['c'], str(params['z'])]) + api_key).hexdigest()\
+            == generate_signature(params, api_key)
