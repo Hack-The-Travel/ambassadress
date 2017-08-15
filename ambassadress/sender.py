061d01c1dcfa7ddcd548ea370108eceb415d1fca
Sergey Popinevskiy <gurza000@gmail.com>
2017-08-15 11:25:05 +0300

    Implemented caller to GET API

--- ambassadress/sender.py
+++ ambassadress/sender.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import requests
-from utils import now
+from utils import generate_signature, now
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -10,7 +10,7 @@ basedir = os.path.dirname(os.path.abspath(__file__))
 
 class SmsClient(object):
     def __init__(self, login, api_key, sender='REDSMS.RU'):
-        self.__gateway = 'http://lik.redsms.ru/'
+        self.__gateway = 'https://lk.redsms.ru/get/'
         self.__login = login
         self.__api_key = api_key
         self.__sender = sender
@@ -22,16 +22,19 @@ class SmsClient(object):
     def _sync_time(self):
         """Set __timestamp property."""
         self.__timestamp = now()
-
-    def get_time(self):
-        """Returns the value of __timestamp property.
-
-         The return value was calculated now (in case there was no API calls before)
-         or was calculated in processing of the last API call.
-         """
-        if self.__timestamp is None:
-            self.__sync_time()
         return self.__timestamp
+
+    def _call(self, service, params=None):
+        if params is not None:
+            request_params = params.copy()
+        else:
+            request_params = dict()
+        request_params['login'] = self.__login
+        request_params['timestamp'] = self._sync_time()
+        signature = generate_signature(request_params, self.__api_key)
+        request_params['signature'] = signature
+        r = requests.get(self.__gateway + service, params=request_params)
+        return r.content
 
     def __call(self, template_filename, context):
         context['security_token'] = self.__token
