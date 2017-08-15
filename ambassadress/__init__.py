6d426c9a40331e34262bc16adec39d8bc6fc8df7
Sergey Popinevskiy <gurza000@gmail.com>
2017-08-15 12:47:42 +0300

    Cleanup. Ready to release 0.0.1 version.

--- ambassadress/__init__.py
+++ ambassadress/__init__.py
@@ -3,15 +3,15 @@
 #  _  _   /  _   _   _  _  _/  _  _   _   _
 # (/ //) () (/ _)  _)  (/ (/  /  (- _)  _)
 
-__title__ = 'ambassadress'
-__version__ = '0.0.0'
-__author__ = 'Sergey Popinevskiy'
-
+from .__version__ import (
+    __title__, __description__, __url__, __version__,
+    __author__, __author_email__, __license__
+)
 from .sender import SmsClient
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-try: # Python 2.7+
+try:  # Python 2.7+
     from logging import NullHandler
 except ImportError:
     class NullHandler(logging.Handler):
--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-import re
+import os
 from codecs import open
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
 
-with open('ambassadress/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Cannot find version information')
+about = dict()
+with open(os.path.join(here, 'ambassadress', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
 
 setup(
-    name='ambassadress',
-    version=version,
-    description='Client to send SMS messages',
-    url='https://github.com/Hack-The-Travel/ambassadress',
-    author='Sergey Popinevskiy',
-    author_email='sergey.popinevskiy@gmail.com',
-    license='',
-    packages=['ambassadress'],
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=about['__description__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=[
+        'ambassadress',
+    ],
+    license=about['__license__'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'requests==2.18.1',
+        'requests>=2.18.0, <3',
     ],
 )
